3a5e0d24a9b8a973286fc7c60972440da105ef73
houssamtermoussi <houssamtermoussi@gmail.com>
2026-06-21 12:58:30 +0100

    use factory

--- app/Models/Eleve.php
+++ app/Models/Eleve.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Eleve extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['nom','prenom','age','classe_id'];
 
     public function classe()
